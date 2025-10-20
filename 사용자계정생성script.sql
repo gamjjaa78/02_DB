91b70dbecc11c7f03bf87bd03bbb979c2fdf006c
gamjjaa78 <prnme7@gmail.com>
2025-10-20 17:43:44 +0900

    function

--- 사용자계정생성script.sql
+++ 사용자계정생성script.sql
@@ -8,17 +8,17 @@
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 --11G 이전 문법 사용 허용
 
-CREATE USER kh_chj IDENTIFIED BY kh1234;
+CREATE USER workbook IDENTIFIED BY workbook;
 --새로운 사용자 계정 생성 (sys:최고 관리자 계정)
 --계정 생성 구문 (kh_chj : USERNAME / kh1234 : PASSWORD)
 --ORA-65096: 공통 사용자 또는 롤 이름이 부적합합니다.
 
-GRANT RESOURCE, CONNECT TO kh_chj;
+GRANT RESOURCE, CONNECT TO workbook;
 --사용자 계정에 권한 부여 설정
 --RESOURCE : 테이블이나 인덱스 같은 DB 객체 생성할 권한
 --CONNECT : DB에 연결하고 로그인 할 수 있는 권한
 
-ALTER USER kh_chj DEFAULT TABLESPACE SYSTEM QUOTA
+ALTER USER workbook DEFAULT TABLESPACE SYSTEM QUOTA
 UNLIMITED ON SYSTEM;
 --객체가 생성될 수 있는 공간 할당량 무제한 지정
 
