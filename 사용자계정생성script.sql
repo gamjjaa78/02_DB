83b504abf3982eea0f6c06f4edf2f8b71fae3b0d
gamjjaa78 <prnme7@gmail.com>
2025-11-20 16:50:07 +0900

    최종

--- 사용자계정생성script.sql
+++ 사용자계정생성script.sql
@@ -8,17 +8,17 @@
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 --11G 이전 문법 사용 허용
 
-CREATE USER kh_shop IDENTIFIED BY kh_shop;
+CREATE USER todo_server IDENTIFIED BY todo1234;
 --새로운 사용자 계정 생성 (sys:최고 관리자 계정)
 --계정 생성 구문 (kh_chj : USERNAME / kh1234 : PASSWORD)
 --ORA-65096: 공통 사용자 또는 롤 이름이 부적합합니다.
 
-GRANT RESOURCE, CONNECT TO kh_shop;
+GRANT RESOURCE, CONNECT TO todo_server;
 --사용자 계정에 권한 부여 설정
 --RESOURCE : 테이블이나 인덱스 같은 DB 객체 생성할 권한
 --CONNECT : DB에 연결하고 로그인 할 수 있는 권한
 
-ALTER USER kh_shop DEFAULT TABLESPACE SYSTEM QUOTA
+ALTER USER todo_server DEFAULT TABLESPACE SYSTEM QUOTA
 UNLIMITED ON SYSTEM;
 --객체가 생성될 수 있는 공간 할당량 무제한 지정
 
